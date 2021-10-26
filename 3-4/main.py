caab870b0c48be63af5e7a968ea60caaa01ecfd1
miharukoma <komatan.makimaki@outlook.jp>
2021-10-26 09:50:19 +0900

    Create main.py

---
+++ 3-4/main.py
@@ -0,0 +1,7 @@
+kazu1=str(input("数を入力してください"))
+kazu2=str(input("数を入力してください"))
+reversed = kazu1[::-1]
+reversed2 = kazu2[::-1]
+reversed = int(reversed)
+reversed2 = int(reversed2)
+print(reversed+reversed2)
